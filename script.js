1f91064dca8d039bc39daccb0af86f634c02edbb
felipevsc <felipevasconceloslpz@gmail.com>
2019-10-13 02:18:15 -0300

    falta singlepage

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-$(function () {
-    $('[data-toggle="popover"]').popover()
-  })
-  
-$('#modalExemplo').on('shown.bs.modal', function (event) {
-  $("#testebotao").trigger("click");  
-});
-
-  $("#testebotao").on("click",function(){
-  console.log("button inside modal clicked");
-  
-  
-  });
-
-function mudarTexto(idTexto){
-  let atual = document.getElementById('textoModel');
-  atual.innerText = idTexto;
-
-};
-
-function mudarImagem(idImagem){
-  document.getElementById("imgIcone").src="icons/"+idImagem+".png";
-  
-}
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -31,43 +7,84 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 };
 
-// FIREBASE REALTIMEDATABASE
-
-var database = firebase.database();
-
-// CRIAR
-
-function writeUserData(userId, name, email, imageUrl) {
-  firebase.database().ref('users/' + userId).set({
-    username: name,
-    email: email,
-    profile_picture : imageUrl
-  });
-}
-
-function writeNewPost(uid, username, picture, title, body) {
-  // A post entry.
-  var postData = {
-    author: username,
-    uid: uid,
-    body: body,
-    title: title,
-    starCount: 0,
-    authorPic: picture
-  };
-
-  // Get a key for a new Post.
-  var newPostKey = firebase.database().ref().child('posts').push().key;
-
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  var updates = {};
-  updates['/posts/' + newPostKey] = postData;
- 
-
-  return firebase.database().ref().update(updates);
-}
-
 function nextPage(idActualDiv, idNextDiv) {
   document.getElementById(idActualDiv).classList.add('hidden')
   document.getElementById(idNextDiv).classList.remove('hidden')
+};
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB2_RDWk68PRh8Y58MJ0CbN8BkuPNTh1JU",
+  authDomain: "agentetein.firebaseapp.com",
+  databaseURL: "https://agentetein.firebaseio.com",
+  projectId: "agentetein",
+  storageBucket: "agentetein.appspot.com",
+  messagingSenderId: "166401056761",
+  appId: "1:166401056761:web:fd693f486b88eb13eda728"
+};
+
+firebase.initializeApp(firebaseConfig);
+var firestore = firebase.firestore();
+
+
+
+
+// VARIAVEIS CADASTRO //
+if (document.getElementById("cadastroUsuario").class.indexOf("hidden")!=-1){
+  var btCadastrar = document.getElementById("btCadastrar");
+
+  btCadastrar.addEventListener("click", function(){
+    console.log("clicado");
+    document.getElementById("emailUsuario").value == "";
+    var emailUsuario = document.getElementById("emailUsuario").value;
+    var docRef = firestore.collection("users").doc(emailUsuario); 
+    
+    if(document.getElementById("senhaUsuario").value == document.getElementById("senhaUsuarioRepetir").value){
+      docRef.set({
+        nomeUsuario: document.getElementById("nomeUsuario").value,
+        emailUsuario: document.getElementById("emailUsuario").value,
+        senhaUsuario: document.getElementById("senhaUsuario").value,
+        numSusUsuario: document.getElementById("numSusUsuario").value,
+        cepUsuario: document.getElementById("cepUsuario").value,
+        numeroUsuario: document.getElementById("numeroUsuario").value,
+        enderecoUsuario: document.getElementById("enderecoUsuario").value
+        
+    
+      })
+    }
+    else{
+      alert("senha errada");
+    }
+  });
 }
+else{
+// login user
+console.log("teste");
+var btLogar = document.getElementById("btLogar");
+btLogar.addEventListener("click", function(){
+  document.getElementById("emailLogin") == "";
+  
+  var emailLogin = document.getElementById("emailLogin").value;
+  var senhaLogin = document.getElementById("senhaLogin").value;
+  var docRefLogin = firestore.collection("users").doc(emailLogin);
+  console.log("testado");
+  docRefLogin.get().then(function(doc) {
+      if (doc && doc.exists){
+        const myData = doc.data();
+        if (myData.senhaUsuario == senhaLogin){
+          console.log(myData.emailUsuario);
+        }
+        else{
+          console.log("senha errada");
+        }
+      }
+    }).catch(function (error){
+      console.log("error");
+    })
+  });
+}
+
+
+
+
+
+
